e37b3df58b0a5e17a957cb50002af31a48526b0c
Simon T <duy.ta1895@gmail.com>
2014-04-05 19:40:45 -0500

    Revert "PA4 initial commit with comments"
    This reverts commit a5eca5e2070b17f2433f5e84c1b802c736111eaa.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,125 @@
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-		//declare int lines and errors to be use as a counter variable
-		int lines = 0, errors = 0;
+	public static void main(String args []) throws FileNotFoundException {
+		//ArrayList list to store all email
+		ArrayList<EmailData> list = new ArrayList<EmailData>();
 		
-		//declare and define a FileIO variable to handle all I/O operations
-		FileIO a = new FileIO("input.txt", "output.txt");
-		a.write("Name: Duy Ta\nPeoplesoft #: 1225688");
+		//reading email from inputemails.txt
+		readEmail(list);
 		
-		//declare a temp IP variable to check IP validity and writing to output.txt
-		IP temp;
+		//writing the specific type of email requested to outputemails.txt
+		writeEmail(list, getOutputCode());
+	}
+	
+	//read file content and pull out all email address and store it in list
+	public static void readEmail(ArrayList<EmailData> list) throws FileNotFoundException{
+		//Scanner to read from file
+		Scanner in = new Scanner(new FileInputStream("inputemails.txt"));
 		
-		//loop to read through output.txt
-		while(a.hasNext()) {
-			//define variable temp
-			temp = new IP(a.read());
-			
-			//increase lines by 1
-			lines++;
-			
-			//if temp is not a valid IP then the increase errors by 1
-			if(temp.isValidIP() == false)
-				errors++;
-			
-			//write variable temp to output.txt
-			a.write(temp.toString());
+		//split email into userName(us), subDomain(sd), domain(d), extension(e)
+		String currentWord, us, sd, d, e;
+		
+		//while there is content in the file, continue to read
+		while(in.hasNext()) {
+			currentWord = us = sd = d = e = null;
+			currentWord = in.next();			
+			//if the current currentWord contain '@', then it is consider as an email
+			if(currentWord.contains("@"))
+			{
+				//assigning userName
+				us = currentWord.substring(0, currentWord.indexOf("@"));
+				
+				//assigning extension
+				if(currentWord.endsWith(","))
+					e = currentWord.substring(currentWord.lastIndexOf(".") + 1, currentWord.length() - 1);
+				else
+					e = currentWord.substring(currentWord.lastIndexOf(".") + 1, currentWord.length());
+				
+				//assigning subDomain & domain 
+				int periodCount = 0;
+				for(int i = currentWord.indexOf("@"); i < currentWord.length(); i++)
+					if(currentWord.charAt(i) == '.')
+						periodCount++;
+				
+				//if there is one period in the currentWord then it is consider as Email, if there is two period in the currentWord then it is consider as UniversityEmail
+				if(periodCount == 1) {
+					d = currentWord.substring(currentWord.indexOf("@") + 1, currentWord.indexOf('.'));
+					list.add(new EmailData(new Email(us, d, e)));
+				} else if(periodCount == 2) {
+					sd = currentWord.substring(currentWord.indexOf("@") + 1, currentWord.indexOf('.'));
+					d = currentWord.substring(currentWord.indexOf('.') + 1, currentWord.lastIndexOf('.'));
+					list.add(new EmailData(new UniversityEmail(us, sd, d, e)));
+				}
+			}
 		}
 		
-		//writing summary to output.txt and closing all I/O operations
-		a.write("\n\nSummary:\nLines read: " + lines + "\nNumber of Errors: " + errors);
-		a.closeFileIO();
+		//closing the file
+		in.close();
+	}
+	
+	public static void writeEmail(ArrayList<EmailData> list, int code) throws FileNotFoundException {
+		//PrintWriter to write to file
+		PrintWriter out = new PrintWriter(new FileOutputStream("outputemails.txt"));
+		String output = "";
+		int num = 0;
+		if(code == 0) {
+			//find all Email and store the Email content in output
+			for(int i = 0; i < list.size(); i++) {
+				EmailData temp = list.get(i);
+				if(temp.isUniversityEmail() == false) {
+					Email te = temp.getE();
+					output += te + "\n";
+					num++;
+				}
+			}
+			
+			//writing to file
+			System.out.println(output);
+			out.println(num);
+			out.println(output);
+		} else {
+			//find all corresponding department and store the UniversityEmail content in output
+			for(int i = 0; i < list.size(); i++) {
+				EmailData temp = list.get(i);
+				if(temp.isUniversityEmail() == true) {
+					UniversityEmail tue = temp.getUE();
+					if(tue.getCode() == code) {
+						output += tue + "\n";
+						num++;
+					}
+				}
+			}
+			
+			//writing to file
+			System.out.println(output);
+			out.println(num);
+			out.println(output);
+		}
+		
+		//closing the file
+		out.close();
+		
+		System.out.println("Sucessfully written to outputemails.txt");
+	}
+	
+	public static int getOutputCode() {
+		//Scanner to get input from user
+		Scanner codeInput = new Scanner(System.in);
+		int code;
+		
+		//prompting user to enter a number from 0 to 7 inclusive
+		System.out.println("Please enter the type of email to write to the text file. (Range from 0 to 7)");
+		code = codeInput.nextInt();
+		while(!(code >= 0 && code <= 7)) {
+			System.err.println("Invalid input! Please enter a number from 0 to 7.");
+			code = codeInput.nextInt();
+		}
+		return code;
 	}
 }
